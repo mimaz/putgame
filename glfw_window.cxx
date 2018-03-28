bd3c0ee1d58d25ced243b5f39804c4f584155461
Mieszko <mimaz@gmx.com>
2018-03-28 05:02:17 +0200

    glfw_window with no internal class

--- glfw_window.cxx
+++ glfw_window.cxx
@@ -10,122 +10,94 @@
 
 #include "glfw_window.hxx"
 
-class glfw_window::internal
-{
-public:
-    static internal *from_window(GLFWwindow *win)
-    {
-        auto ptr = glfwGetWindowUserPointer(win);
-
-        return reinterpret_cast<internal *>(ptr);
-    }
-
-    static void key_callback(GLFWwindow *win,
-                             int key, int code,
-                             int action, int mode)
-    {
-        from_window(win)->key(key, action);
-    }
-
-    static void resize_callback(GLFWwindow *win,
-                                int width, int height)
-    {
-        from_window(win)->resize(width, height);
-    }
-
-    internal(int width, int height, const std::string &title)
-        : win(nullptr)
-        , ctx(nullptr)
-    {
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-        glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_NATIVE_CONTEXT_API);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-
-        win = glfwCreateWindow(width, height, title.c_str(), 
-                               nullptr, nullptr);
-
-        if (win == nullptr)
-            throw glfw_window::exception("creating glfw window failed ");
-
-        ctx = std::make_unique<common::context>();
-
-        glfwSetWindowUserPointer(win, this);
-        glfwSetKeyCallback(win, key_callback);
-        glfwSetWindowSizeCallback(win, resize_callback);
-
-        glfwMakeContextCurrent(win);
-
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glFrontFace(GL_CW);
-
-        btn = std::make_unique<gui::color_button>(ctx.get());
-        btn->resize(400, 400);
-    }
-
-    ~internal()
-    {
-        if (win != nullptr)
-            glfwDestroyWindow(win);
-    }
-
-    void resize(int width, int height)
-    {
-        std::cout << "resize: " << width << " x " << height << std::endl;
-        glViewport(0, 0, width, height);
-
-        ctx->get_part<gui::surface>()->resize(width, height);
-    }
-
-    void key(int key, int action)
-    {
-        if (action != GLFW_RELEASE)
-            return;
-
-        switch (key)
-        {
-            case GLFW_KEY_ESCAPE:
-                glfwSetWindowShouldClose(win, GLFW_TRUE);
-                break;
-        }
-    }
-
-    void draw()
-    {
-        try {
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-            btn->draw();
-        } catch (glutils::location_error e) {
-            std::cerr << "location error: " << e.name << std::endl;
-        }
-
-        glfwSwapBuffers(win);
-        glfwPollEvents();
-    }
-
-    GLFWwindow *win;
-
-    std::unique_ptr<common::context> ctx;
-    std::unique_ptr<gui::color_button> btn;
-};
-
 glfw_window::glfw_window(int width, int height,
                          const std::string &title)
-    : priv(new internal(width, height, title))
-{}
+    : win(nullptr)
+{
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+    glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_NATIVE_CONTEXT_API);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    win = glfwCreateWindow(width, height, title.c_str(), 
+                           nullptr, nullptr);
+
+    if (win == nullptr)
+        throw std::string("creating glfw window failed ");
+
+    glfwSetWindowUserPointer(win, this);
+    glfwSetKeyCallback(win, key_callback);
+    glfwSetWindowSizeCallback(win, resize_callback);
+
+    glfwMakeContextCurrent(win);
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glFrontFace(GL_CW);
+}
 
 glfw_window::~glfw_window()
 {
-    delete priv;
+    glfwDestroyWindow(win);
 }
 
 void glfw_window::draw()
 {
-    priv->draw();
+    try {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        //btn->draw();
+    } catch (glutils::location_error e) {
+        std::cerr << "location error: " << e.name << std::endl;
+    }
+
+    glfwSwapBuffers(win);
+    glfwPollEvents();
 }
 
 bool glfw_window::should_close() const
 {
-    return glfwWindowShouldClose(priv->win);
+    return glfwWindowShouldClose(win);
+}
+
+glfw_window *glfw_window::from_window(GLFWwindow *win)
+{
+    auto ptr = glfwGetWindowUserPointer(win);
+
+    return reinterpret_cast<glfw_window *>(ptr);
+}
+
+void glfw_window::key_callback(GLFWwindow *win,
+                               int key, int code,
+                               int action, int mode)
+{
+    from_window(win)->key(key, action);
+}
+
+void glfw_window::resize_callback(GLFWwindow *win,
+                                  int width, int height)
+{
+    from_window(win)->resize(width, height);
+}
+
+void glfw_window::key(int key, int action)
+{
+    if (action != GLFW_RELEASE)
+        return;
+
+    switch (key)
+    {
+        case GLFW_KEY_ESCAPE:
+            glfwSetWindowShouldClose(win, GLFW_TRUE);
+            break;
+
+        default:
+            break;
+    }
+}
+
+void glfw_window::resize(int width, int height)
+{
+    glViewport(0, 0, width, height);
+
+    get_part<gui::surface>()->resize(width, height);
 }
--- glfw_window.hxx
+++ glfw_window.hxx
@@ -6,32 +6,31 @@
 #ifndef __glfw_window_hxx
 #define __glfw_window_hxx
 
-class glfw_window
+#include <putgame/common>
+
+class glfw_window : public common::context
 {
 public:
-    class exception;
-    class internal;
-
     glfw_window(int width, int height,
                 const std::string &title = "putgame");
 
     ~glfw_window();
 
     void draw();
-
     bool should_close() const;
 
 private:
-    internal *priv;
-};
+    static glfw_window *from_window(GLFWwindow *win);
+    static void key_callback(GLFWwindow *win,
+                             int key, int code,
+                             int action, int mode);
+    static void resize_callback(GLFWwindow *win,
+                                int width, int height);
 
-class glfw_window::exception
-{
-public:
-    exception(const std::string &desc)
-        : desc(desc) {}
+    void key(int key, int action);
+    void resize(int width, int height);
 
-    std::string desc;
+    GLFWwindow *win;
 };
 
 #endif
