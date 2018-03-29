68162c4ca0a76f5723c1cfbf8c0b74f01eb11342
Mieszko <mimaz@gmx.com>
2018-03-29 19:17:42 +0200

    improved way path

--- world/way_path.cxx
+++ world/way_path.cxx
@@ -8,16 +8,34 @@
 
 #include "way_path.hxx"
 
+namespace
+{
+    class default_generator : public world::way_path::generator
+    {
+        std::pair<float, glm::vec3> generate() override
+        {
+            return std::make_pair(PI / 80, glm::vec3(0, 1, 0));
+        }
+    };
+}
+
 namespace world
 {
     way_path::way_path(common::context *ctx)
         : common::context::part(ctx)
         , path_line(1)
+        , gen(std::make_shared<default_generator>())
     {}
+
+    void way_path::set_generator(shared_generator gn)
+    {
+        gen = gn;
+    }
 
     void way_path::generate()
     {
-        append(PI / 80, { 0, 1, 0 });
-        // TODO generate default path
+        auto [angle, axis] = gen->generate();
+
+        append(angle, axis);
     }
 }
--- world/way_path.hxx
+++ world/way_path.hxx
@@ -15,10 +15,39 @@ namespace world
     class way_path : public common::context::part, public path_line
     {
     public:
+        class generator;
+
+        using shared_generator = std::shared_ptr<generator>;
+
+
         way_path(common::context *ctx);
 
+        void set_generator(shared_generator gen);
+
+          template<typename _Type, typename ..._Args>
+        void set_generator(const _Args &...args);
+
         void generate();
+
+    private:
+        shared_generator gen;
     };
+
+    class way_path::generator
+    {
+    public:
+        virtual ~generator() {}
+
+        virtual std::pair<float, glm::vec3> generate() = 0;
+    };
+
+      template<typename _Type, typename ..._Args>
+    void way_path::set_generator(const _Args &...args)
+    {
+        auto gen = std::make_shared<_Type>(args...);
+
+        set_generator(gen);
+    }
 }
 
 #endif
